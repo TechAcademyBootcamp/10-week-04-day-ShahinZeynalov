c3fefe14a6f1fcb46f01b046b0379cdcf32041ab
Shahin Zeynalov <shahin.zeynalov@bk.ru>
2019-12-13 00:12:55 +0400

    12.12.2019

---
+++ Carousel images/.venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shahin/Documents/Tech_Homeworks/10-week-04-day-ShahinZeynalov/Carousel images/.venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Carousel images/carousel_project/static/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        smartSpeed:500,
+        nav:true,
+        dots:false,
+        autoPlayHoverPause:true,
+
+    });
+});
